e365c0ee4f511ad983dd4f6eb04193ae29f48c5c
Arfan Marwali <marwalia@oregonstate.edu>
2018-06-15 00:32:28 -0700

    homepage complete minus search function

--- home.js
+++ home.js
@@ -29,34 +29,44 @@ cancelBtn.addEventListener('click', function (){
 var acceptBtn = document.getElementsByClassName('modal-accept-button')[0];
 acceptBtn.addEventListener('click', function (event){
    if((profilePicture.value == "") && (profileName.value == "")){
-      alert("You did not enter a value for Text or Author");
+      alert("You did not enter a value for picture or name");
       return;
    }
 
    if (profilePicture.value == ""){
-      alert("You did not enter a value for Text");
+      alert("You did not enter a value for picture");
       return;
    }
 
    if(profileName.value == ""){
-      alert("You did not enter a value for Author");
+      alert("You did not enter a value for name");
       return;
    }
-   var x = document.getElementsByClassName('profile')[0];
-   var xPrime = x.cloneNode(true);
-   var y = document.getElementsByClassName('profile-container')[0];
-   y.appendChild(xPrime);
-   
-   var z = document.getElementsByClassName('profile');
-   var a = document.getElementsByClassName('profile-name')[z.length-1];
-   a.href ='#';
-   var b = document.getElementsByClassName('profile-picture')[z.length-1];
 
-   console.log(a.href);
-   a.textContent=profileName.value;
-   console.log(a.href);
-   b.src=profilePicture.value;
-   console.log(b);
+  var picture = document.createElement("IMG");
+  picture.classList.add('profile-picture');
+  picture.src = profilePicture.value;
+
+  var author = document.createElement('a');
+  author.href = '#';
+  author.textContent = profileName.value;
+
+  var attribution = document.createElement('p');
+  attribution.classList.add('profile-name');
+  attribution.appendChild(author);
+
+  var profileContent = document.createElement('div');
+  profileContent.classList.add("profile-content");
+  profileContent.appendChild(picture);
+  profileContent.appendChild(attribution);
+
+
+  var profile = document.createElement('article');
+  profile.classList.add('profile');
+  profile.appendChild(profileContent);
+  
+  var body = document.getElementsByClassName('profile-container')[0];
+  body.appendChild(profile);
 
    profilePicture.value = "";
    profileName.value = "";						    
@@ -74,11 +84,11 @@ searchBar.addEventListener('keyup', search);
 function search(event){
    var profiles = document.getElementsByClassName('profile');
    for (i=0; i<profiles.length; i++) {
-   //var a=profiles[i].childNodes[3].childNodes[1].textpicture.toLowerCase;
-   //var b=profiles[i].childNodes[3].childNodes[3].textpicture.toLowerCase;  //attempted to make it lower case but it wouldnt work
+   //var a=profiles[i].childNodes[3].childNodes[1].picturepicture.toLowerCase;
+   //var b=profiles[i].childNodes[3].childNodes[3].picturepicture.toLowerCase;  //attempted to make it lower case but it wouldnt work
    //var c=searchBar.value.toLowerCase;
       //if((a.includes(c)) || (b.includes(c))){
-      if ((profiles[i].childNodes[3].childNodes[1].textpicture.includes(searchBar.value)) || (profiles[i].childNodes[3].childNodes[3].textpicture.includes(searchBar.value))) {
+      if ((profiles[i].childNodes[3].childNodes[1].picturepicture.includes(searchBar.value)) || (profiles[i].childNodes[3].childNodes[3].picturepicture.includes(searchBar.value))) {
          profiles[i].classList.remove('hidden');
          continue;
       }
